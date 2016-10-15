d9173f48a93a9d07f9a1c0a59171b13add8d7936
pihizi <pihizi@msn.com>
2016-10-15 15:49:15 +0000

    修改配置

--- sites/node/config/database.php
+++ sites/node/config/database.php
@@ -1,7 +1,7 @@
 <?php
 
 
-$config['root']='mysql://genee:83719730@172.17.0.1/%database';
+$config['root']='mysql://genee:83719730@{{{DOCKER0IP}}}/%database';
 $config['prefix'] = 'mall_';
 
 $config['@sphinx.url']='mysql://sphinx:9306/%database';
--- sites/node/config/debade.php
+++ sites/node/config/debade.php
@@ -2,11 +2,11 @@
 
 $config['queues'] = [
     'order' => [
-        'addr' => 'tcp://172.17.0.1:3333',
+        'addr' => 'tcp://{{{DOCKER0IP}}}:3333',
         'queue' => 'mall',
     ],
     'transfer_statement' => [
-        'addr' => 'tcp://172.17.0.1:3333',
+        'addr' => 'tcp://{{{DOCKER0IP}}}:3333',
     	'queue' => 'mall',
     ],
 ];
